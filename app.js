44933bc357506be86bf47eff7c6a389c687ee740
emersoo1 <124084232+emersoo1@users.noreply.github.com>
2024-11-08 23:15:09 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUEwfAl_zV1jh46GROk5_3UwYN3pbTYKs",
+  authDomain: "vagas-capixaba.firebaseapp.com",
+  projectId: "vagas-capixaba",
+  storageBucket: "vagas-capixaba.firebasestorage.app",
+  messagingSenderId: "843279287692",
+  appId: "1:843279287692:web:81a99238c393b5869a82a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
